75a81281b959f0afcb9fe27e0515265974f438b7
JWL4402 <j.lebert4402@gmail.com>
2023-08-05 19:32:03 -0400

    added a barebone crop scriptable object

---
+++ mfgjs2023/Assets/Crops/Crop.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crop : ScriptableObject
+{
+    public int value;
+    public int price;
+    public float growthRate;
+}
