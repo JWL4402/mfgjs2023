479a5b4c8695bd7c988920e0ab6ad307f84e8284
JWL4402 <j.lebert4402@gmail.com>
2023-08-05 18:02:43 -0400

    added basic field functionality. barebones debris system.

---
+++ mfgjs2023/Assets/Scripts/PlotScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotScript : MonoBehaviour
+{
+    public enum DebrisState { NONE, LIGHT, MEDIUM, HEAVY  };
+
+    public DebrisState debris;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
