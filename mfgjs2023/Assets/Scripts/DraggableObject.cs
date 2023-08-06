db8b59541f607efc40d54f8f29a81646d40e45fe
JWL4402 <j.lebert4402@gmail.com>
2023-08-05 20:18:52 -0400

    refined the DraggableObject.cs class

--- mfgjs2023/Assets/Scripts/DraggableObject.cs
+++ mfgjs2023/Assets/Scripts/DraggableObject.cs
@@ -10,8 +10,7 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class DraggableObject : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    public bool generator;
-    public GameObject copiedObject;
+    public GameObject draggedObject;
     public GameObject clone;
 
     void Start()
@@ -21,7 +20,7 @@ public class DraggableObject : MonoBehaviour, IPointerDownHandler, IPointerUpHan
 
     void Update()
     {
-        if (clone == null) { return;  }
+        if (clone == null) { return; }
 
         Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         clone.transform.position = mouse_pos;
@@ -42,50 +41,18 @@ public class DraggableObject : MonoBehaviour, IPointerDownHandler, IPointerUpHan
         }
     }
 
-    /// <summary>
-    /// Generates a clone of the Clickable object to be moved. Used for Generator functionality.
-    /// </summary>
     private void GenerateClone()
     {
-        clone = Object.Instantiate(copiedObject, gameObject.transform.parent);
-
-        DraggableObject script = clone.GetComponent<DraggableObject>();
-        if (script == null) { Debug.LogError("Generator generated an object without script."); }
-        // if it's copy of object with this script, should have this script
-
-        script.generator = false;
+        clone = Object.Instantiate(draggedObject, gameObject.transform.parent);
     }
-
-    /// <summary>
-    /// Trashes an object if it is dropped onto its Trash Object.
-    /// </summary>
-    /// <param name="event_pos">The 2D position of the click.</param>
-    /// <returns>True if trashed, false otherwise.</returns>
-    //private bool CheckTrashed(Vector2 event_pos)
-    //{
-    //    if (Trash_Object == null) { return false; }
-
-    //    Vector3 click_pos = Camera.main.ScreenToWorldPoint(event_pos);
-    //    RaycastHit2D[] ray_hits = Physics2D.GetRayIntersectionAll(new Ray(click_pos, transform.forward));
-
-    //    foreach (RaycastHit2D hit in ray_hits)
-    //    {
-    //        if (hit.collider.gameObject == Trash_Object)
-    //        {
-    //            Destroy(gameObject);
-    //            return true;
-    //        }
-    //    }
-    //    return false;
-    //}
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (generator) { GenerateClone(); }
+        GenerateClone();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        if (generator) { Destroy(clone); }
+        Destroy(clone);
     }
 }
